ea3e1b2ce50f7f03c5dd636ebaa1c53ac66899b7
tweek <tja.theau54@gmail.com>
2019-07-08 13:44:06 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# TODO JS
+
+Dans le champ input, une valeur **numérique** sera entrée (par exemple `48569971`).
+Lorsque l'on clique sur le bouton, le modal s'ouvre.
+
+Le but de cet exercice est le suivant :
+
+- La valeur de l'input de la calculatrice **doit** être la même que celle présente dans le champ.
+- À chaque opération, l'utilisateur doit visualiser ces opérations dans l'input de la calculatrice. (exemple: si il additionne 3 à 5, il doit voir `3+5`)
+- Au clic sur "**Remplacer**", la nouvelle valeur doit venir écraser l'ancienne.
---
+++ script.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+	
+	const insert = (num) => {
+		$(".myChampsCalc").val($(".myChampsCalc").val() + num);
+	}
+	const total = () => {
+		let exp = $(".myChampsCalc").val();
+		$(".myChampsCalc").val(eval(exp));
+	}
+
+
+	const clear = () => {
+		$(".myChampsCalc").val('');
+	}
+
+	const replace = (val) => {
+
+	}
+
+	$('.button-addon2').click(function () 
+	{
+		let val = $(this).parent().parent().children('.oldVal').val();
+		$(this).parent().parent().children('.oldVal').addClass('calculetteOn');
+		$(".myChampsCalc").val(val);
+		
+		
+		
+	});
+	$('.replace').click(function () { 
+		let exp = $(".myChampsCalc").val();
+		let val = eval(exp);
+		$('#coulcoulette').modal('hide');		
+		
+		$('.calculetteOn').val(val);
+
+		$('.calculetteOn').removeClass('calculetteOn');
+	});
+	
+	$('.val').off().on('click', function () { 
+		let num = $(this).val();
+		console.log($(this).val());
+		insert(num);
+
+		if ($(this).val() === 'C') {
+			clear();
+		}
+	});
+
+	$('.totaux').click(function () {
+		total();
+	});
+
+});
